625972346240fa0c85f8ddbaaf9492b8e37d4363
drunknzombiecow <nils.roehrig@fh-duesseldorf.de>
2014-02-18 13:25:22 +0100

    oauth examples added

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'rJwaBrdvVYfqVcu8ZvTBaw');
+define('CONSUMER_SECRET', 'pJeyXDmZzYNUTetag0p6bqlhXE2ZhYFR8j4aHf2E69w');
+define('OAUTH_CALLBACK', 'callback.php');
